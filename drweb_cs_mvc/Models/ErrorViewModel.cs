9b1595e53064345559fe9984060bd0ca488b0145
User <User@DESKTOP-1BNTKFS>
2024-03-01 12:39:08 +0800

    加入專案檔案。

---
+++ drweb_cs_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace drweb_cs_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
